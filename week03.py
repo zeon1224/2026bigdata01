2d41d6a13767d5c2603669acf26dae3ad6a15785
zeon1224 <jjy0477@gmail.com>
2026-03-19 15:12:41 +0900

    week03 v0.6

--- week03.py
+++ week03.py
@@ -1,7 +1,4 @@
-
-import numpy
-#써드파티 라이브러리, C 배열 기반을 ㅗ대규모 데이터 처리에 적합. 매우빠름
-
-score = [100, 90, 88, 91]
-average = numpy.mean(score)
+import pandas as pd
+scores = [100, 97 ,88 ,91]
+average = pd.Series(scores).mean()
 print(average)
