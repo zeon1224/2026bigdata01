b182bc73b2332cc378d2a8004223fca571a4ea3a
zeon1224 <jjy0477@gmail.com>
2026-03-19 14:38:35 +0900

    week03 v0.3

--- week02.py
+++ week02.py
@@ -1,9 +1,15 @@
 scores = [100, 97, 88, 91]
-hap = 0
-count = 0
+hap, count = 0, 0
 for score in scores:
     hap = hap + score
     count = count + 1
-
-average = hap / count
 print(average)
+
+#scores = [100, 97, 88, 91]
+#average =
+#print(average)
+
+#import statistics # 통계모듈 (각종 통계함수를 제공)
+#scores = [100, 97, 88, 91]
+#average = statistics.mean(scores)
+#print(average)
---
+++ week03.py
@@ -0,0 +1,7 @@
+
+import numpy
+#써드파티 라이브러리, C 배열 기반을 ㅗ대규모 데이터 처리에 적합. 매우빠름
+
+score = [100, 90, 88, 91]
+average = numpy.mean(score)
+print(average)
