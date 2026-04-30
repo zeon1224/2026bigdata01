b6efca16143fcd556d1aafaa6d47e5b923465f19
zeon1224 <jjy0477@gmail.com>
2026-04-30 16:27:39 +0900

    week09 pandas v0.6

--- week09.py
+++ week09.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 df = pd.DataFrame({'국':[1, 6, 7], '영':[2, 4, 8], '수':[3, 5, 9], '화':[10, 3, 11]}, index=[1, 2, 3])
 print(df)
-df_new = df.loc[df['국']>5, ['영','화']]
-# df_new = df.loc[:,'영':'화']
-# df_new = df.iloc[:,[1, 3]]
-print(df_new)
+print(df.iat[1,2])
+print(df.at[2,'수'])
+print(df.sample(frac=0.33))
+print(df.nsmallest(2,'화'))
+print(df.tail(2))
