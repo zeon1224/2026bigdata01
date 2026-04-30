1a855bb78e9c42fe90408d80033854acadf11309
zeon1224 <jjy0477@gmail.com>
2026-04-30 16:11:29 +0900

    week09 pandas v0.5

--- week09.py
+++ week09.py
@@ -2,8 +2,7 @@ import pandas as pd
 
 df = pd.DataFrame({'국':[1, 6, 7], '영':[2, 4, 8], '수':[3, 5, 9], '화':[10, 3, 11]}, index=[1, 2, 3])
 print(df)
-# df_new = df.iloc[0:3]  # df_new = df.iloc[:]
-# df_new = df.iloc[1:3]
-# df_new = df.iloc[1:]
-df_new = df.iloc[1:,2:]
+df_new = df.loc[df['국']>5, ['영','화']]
+# df_new = df.loc[:,'영':'화']
+# df_new = df.iloc[:,[1, 3]]
 print(df_new)
