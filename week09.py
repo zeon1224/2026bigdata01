0e7f485d3dfdeb74235617dfea316f1793d94a17
zeon1224 <jjy0477@gmail.com>
2026-04-30 15:51:00 +0900

    week09 pandas v0.4

--- week09.py
+++ week09.py
@@ -1,14 +1,9 @@
 import pandas as pd
 
-# df = pd.DataFrame({'국':[1, 6, 7], '영':[2, 4, 8], '수':[3, 5, 9]}, index=[1, 2, 3])
-# print(df)
-# # df_new = pd.melt(df)
-# # df_new = pd.melt(df).rename(columns={'variable': 'var', 'value': 'val'})
-# # df_new = pd.melt(df).rename(columns={'variable': 'var', 'value': 'val'}).query('val > 5')
-# df_new = pd.melt(df).rename(columns={'variable': 'var', 'value': 'val'}).query('val > 5').sort_values('val', ascending=False)
-# print(df_new)
-
-df = pd.DataFrame({'국':[1, 6, 7], '영':[2, 4, 8], '수':[3, 5, 9], '미세먼지':[10, 3, 11]}, index=[1, 2, 3])
+df = pd.DataFrame({'국':[1, 6, 7], '영':[2, 4, 8], '수':[3, 5, 9], '화':[10, 3, 11]}, index=[1, 2, 3])
 print(df)
-df_new = df.drop(columns=['수', '미세먼지'])
+# df_new = df.iloc[0:3]  # df_new = df.iloc[:]
+# df_new = df.iloc[1:3]
+# df_new = df.iloc[1:]
+df_new = df.iloc[1:,2:]
 print(df_new)
