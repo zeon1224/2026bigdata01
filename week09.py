bb91ad217c8bf7f375d395bfdd7307720b4365cc
zeon1224 <jjy0477@gmail.com>
2026-04-30 14:51:08 +0900

    week09 pandas v0.2

--- week09.py
+++ week09.py
@@ -1,19 +1,10 @@
 import numpy as np
 import pandas as pd
 
-arr2d = np.array(
-    [
-        [1,2,3],
-        [4,5,6],
-        [7,8,9]
-    ]
-)
 
-#2차원 리스트
-# df_dict = pd.DataFrame(arr2d, columns=['국', '영', '수'], index=[1,2,3])
-
-df_2dlist = pd.DataFrame([[1,2,3], [4,5,6], [7,8,9]], columns=['국', '영', '수'])
-df_dict = pd.DataFrame({'국':[1,6,7], '영':[2,4,8], '수':[3,5,9]}, index = [1,2,3])
-print (arr2d)
-print (df_2dlist)
-print (df_dict)
+df = pd.DataFrame({'국':[1,6,7], '영':[2,4,8], '수':[3,5,9]}, index = [1,2,3])
+print (df)
+# df_new = pd.melt(df)
+# df_new = pd.melt(df).rename(columns={'variable': 'var', 'value': 'val'})
+df_new = pd.melt(df).rename(columns={'variable': 'var', 'value': 'val'}).query('val >5')
+print (df_new)
