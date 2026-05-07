48c71b025e5667a8ae9d083928ebb955b08ae6a7
zeon1224 <jjy0477@gmail.com>
2026-05-07 16:16:14 +0900

    week10 pandas v1.0

--- week10.py
+++ week10.py
@@ -1,8 +1,13 @@
-import pandas as pd
+import seaborn as sns
 
-def square(x):
-    return x*x
-
-df = pd.DataFrame({'국':[7, 6, 7], '영':[2, 4, 8], '수':[3, 5, 9], '화':[10, 3, 11]}, index=[1, 2, 3])
-print(df)
-print(df.apply(square))
+mpg = sns.load_dataset('mpg')
+# print(mpg.head())
+# print(mpg.info())
+# print(mpg.describe())
+# print(mpg['cylinders'].value_counts())
+print(mpg[mpg['horsepower'].isnull()])
+mpg['horsepower'] = mpg['horsepower'].fillna(
+    mpg.groupby('cylinders')['horsepower'].transform('median')
+)
+print(mpg.info())
+print(mpg[mpg['horsepower'].isnull()])
